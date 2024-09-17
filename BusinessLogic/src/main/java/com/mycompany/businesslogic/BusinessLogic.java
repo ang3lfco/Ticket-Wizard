69ce9729af758a3c31b753a9c732b7af30fd3d58
ang3lfco <ang3lfco@outlook.com>
2024-09-16 22:36:18 -0700

    first commit - Estructura del Proyecto

---
+++ BusinessLogic/src/main/java/com/mycompany/businesslogic/BusinessLogic.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.businesslogic;
+
+/**
+ *
+ * @author martinez
+ */
+public class BusinessLogic {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
